483cce79c1b59a3e270ae89817256d430a91541c
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-16 12:21:46 +0200

    változás, adatok és if eleje

--- MoziJegyKalkulator/Program.cs
+++ MoziJegyKalkulator/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Vásárló neve: ")
-string nev=Console.ReadLine();
-Console.WriteLine($"Vásárló neve: {nev}");
+﻿string nev = "Nagy Zsófia";
+int kor = 16;
+bool diak = true;
+double mozj = 2800;
+double dbsz = 3;
+double pop = 1400;
+float kedvezmeny = 0.20;
+if (kor >= 18)
+{
 
+}
