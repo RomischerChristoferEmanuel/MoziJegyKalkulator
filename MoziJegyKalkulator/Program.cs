bc64f351d88ecf0ab3e79a3a8119f0ae54262061
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-16 12:41:47 +0200

    adatok kiírása + második if

--- MoziJegyKalkulator/Program.cs
+++ MoziJegyKalkulator/Program.cs
@@ -5,9 +5,17 @@ double mozj = 2800;
 double dbsz = 3;
 double pop = 1400;
 float kedvezmeny = 0.20;
-if (kor >= 18)
+if ((kor <= 18) & (diak=true))
 {
     double ar = mozj + (pop * dbsz);
     double vegar = ar / 100 * kedvezmeny;
 
+}
+Console.WriteLine($"Vásárló neve: {nev}  ({kor})");
+Console.WriteLine($"Rendelés 1db Mozijegy + {dbsz} db Popcorn");
+Console.WriteLine($"Alapösszeg {mozj + (pop * dbsz)} Ft");
+Console.WriteLine($"Fizetendő összeg: {mozj + (pop * dbsz) /100 * kedvezmeny}");
+if (diak = true)
+{
+    Console.WriteLine("20% kedvezmény érvényesítve!");
 }
